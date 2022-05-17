e4860fcf93723e9026e1dcffefb7bf48a8851db3
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-05-17 13:24:01 +0200

    Stage 1/4: Rush into print

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(printIntro());
+    }
+
+    private static String printIntro() {
+        return """
+                I love animals!
+                Let's check on the animals...
+                The deer looks fine.
+                The bat looks happy.
+                The lion looks healthy.
+                """;
+    }
+}
